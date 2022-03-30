85e0d00b63f5e9111d94d7487d7e9fbb8efd1a0a
khalil0525 <collinskhalil@hotmail.com>
2022-03-30 06:52:47 -0400

    initial files

---
+++ src/components/form/UserForm.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./UserForm.modules.css";
+
+const UserForm = () => {};
+
+export default UserForm;
---
+++ src/components/list/UserList.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./UserListModules.css";
+
+const UserList = () => {};
+
+export default UserList;
